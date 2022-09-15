c957f222e85c1f5690097771b7f0a3e41273431d
Zach Gonzales <zacharyrgonzales@gmail.com>
2022-09-15 12:17:10 -0500

    init

---
+++ video-stream/src/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const http = require("http");
+const mongodb = require("mongodb");
+const app = express();
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+function main() {
+    return mongodb.MongoClient.connect(DBHOST)
+        .then(client => {
+            const db = client.db(DBNAME);
+            const videosCollection = 
+            db.collection("video");
+        
+            app.get("/video", (req, res) => {
+                const videoId = 
+                new mongodb.ObjectID(req.query.id);
+                videosCollection
+                .findOne({ _id: videoId })
+                    .then(videoRecord => {
+                        if (!videoRecord) {
+                            res.sendStatus(404);
+                            return;
+                        }
+        
+                        const forwardRequest = http.request( 
+                            {
+                                host: VIDEO_STORAGE_HOST,
+                                port: VIDEO_STORAGE_PORT,
+                                path:`/video?path=${videoRecord
+                                .videoPath}`,
+                                method: 'GET',
+                                headers: req.headers
+                            }, 
+                            forwardResponse => {
+                                res.writeHeader(forwardResponse.statusCode, 
+                                forwardResponse.headers);
+                                forwardResponse.pipe(res);
+                            }
+                        );
+                        
+                        req.pipe(forwardRequest);
+                    })
+                    .catch(err => {
+                        console.error("Database query failed.");
+                        console.error(err && err.stack || err);
+                        res.sendStatus(500);
+                    });
+            });
+            
+            app.listen(PORT, () => {
+                console.log(`Microservice online.`);
+            });
+        });
+}
+main()
+    .then(() => console.log("Microservice online."))
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
